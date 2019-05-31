c81a6c04da3b05b42627632acbd45a2706235063
kdwivedi <kdwivedi@contractor.jcp.com>
2019-05-31 15:19:43 +0530

    changes made

--- src/main/java/excelDataFetching/ExcelFetch.java
+++ src/main/java/excelDataFetching/ExcelFetch.java
@@ -17,7 +17,7 @@ public class ExcelFetch {
 	public static void main(String args[])throws IOException{
 		
 		HashMap<String, List<String>> hashMap1 = new HashMap<String, List<String>>();
-		List<String> list ;
+		List<String> list=null ;
 		FileInputStream fis=new FileInputStream("resources/excel.xlsx");
 		@SuppressWarnings("resource")
 		XSSFWorkbook workbook= new XSSFWorkbook(fis);
@@ -31,17 +31,16 @@ public class ExcelFetch {
 			 Iterator<Cell> cellIterator = row.cellIterator();
 			 cellIterator.next();
 			 while(cellIterator.hasNext()) {
-				// Cell cell= cellIterator.next();
 				 Cell cell2= cellIterator.next();
 					switch(cell2.getCellType()) {
+					case STRING: 
+                        String s=cell2.getStringCellValue(); 
+                        list.add(s); 
+                        break;
 					 case NUMERIC: 
                          String values =String.valueOf((int)cell2.getNumericCellValue()); 
                          list.add(values);
                          break; 
-                     case STRING: 
-                        String s=cell2.getStringCellValue(); 
-                        list.add(s); 
-                        break;
 					default:
 						break;
 					}
@@ -49,5 +48,20 @@ public class ExcelFetch {
 				 }
 			}
 		 System.out.println(hashMap1);
+		
+		 for (String student : hashMap1.keySet()) { 
+	            System.out.println("Student: " + student); 
+	            int i=1;
+	        	for(String mark:hashMap1.get(student))
+	         {		if(i==1)
+	        		 System.out.println("Marks in Physics: " + mark); 
+		         if(i==2)
+	        		 System.out.println("Marks in Chemistry: " + mark); 
+		         if(i==3)
+	        		 System.out.println("Marks in Maths: " + mark); 
+		         i++;
+	        	}
+	        }  
+		 System.out.println("Kritika Marks in Physics is="+ hashMap1.get("Kritika"));
 	}	
 }
